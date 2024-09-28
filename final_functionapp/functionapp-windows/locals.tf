3e0a4531d1d70b56a7dc1c260a0e4fcf2fed9614
Ibrahim K <68802843+ibrahimmumgit@users.noreply.github.com>
2024-09-28 15:38:00 +0800

    Update locals.tf

--- final_functionapp/functionapp-windows/locals.tf
+++ final_functionapp/functionapp-windows/locals.tf
@@ -7,6 +7,10 @@ locals {
   effective_environment     = var.create_new_resource_group ? var.environment : local.environment
   effective_applicationname = var.create_new_resource_group ? var.applicationname : local.applicationname
   effective_locaion         = var.create_new_resource_group ? var.location : data.azurerm_resource_group.rg[0].location
+  funcappname = "${var.rg_prefix}-${local.effective_environment == "PROD" ? "1${local.effective_applicationname}func" : local.effective_environment == "UAT" ? "4${local.effective_applicationname}func" : "5${local.effective_applicationname}func"}"
+
+  # Regex pattern to match the funcappname
+  funcappname_pattern = "(?i)${local.funcappname}" # Case insensitive pattern
 
   # Find the Max number for service plan
   service_plan_name                          = [for sp in data.azurerm_resources.app_service_plans.resources : sp.name]
@@ -18,14 +22,14 @@ locals {
   existing_serviceplans_max_resource_numbers = length(local.existing_serviceplans_resource_numbers) > 0 ? max(local.existing_serviceplans_resource_numbers...) : 0
 
 
-  # Find the Max number for functionapp
-  existing_total_azure_funcapp_count     = length(data.azurerm_resources.existing_funcapps.resources)
-  existing_funcapps_resource_numbers_str = length(data.azurerm_resources.existing_funcapps.resources) > 0 ? [for res in data.azurerm_resources.existing_funcapps.resources : regexall("[0-9]+$", res.name)[0]] : []
-  existing_funcapps_resource_numbers     = length(data.azurerm_resources.existing_funcapps.resources) > 0 ? [for num_str in local.existing_funcapps_resource_numbers_str : tonumber(num_str)] : []
+  # Filter Function Apps based on name containing 'func' (case insensitive)
+  filtered_function_apps = [for res in data.azurerm_resources.existing_funcapps.resources : res if can(regex(local.funcappname_pattern, res.name))]
 
-  # Max resource number, defaulting to 0 if not applicable
+  # Extract numerical suffix from Function App names
+  existing_funcapps_resource_numbers_str = length(local.filtered_function_apps) > 0 ? [for res in local.filtered_function_apps : regexall("[0-9]+$", res.name)[0]] : []
+  existing_funcapps_resource_numbers = length(local.existing_funcapps_resource_numbers_str) > 0 ? [for num_str in local.existing_funcapps_resource_numbers_str : tonumber(num_str)] : []
   existing_funcapps_max_resource_numbers = length(local.existing_funcapps_resource_numbers) > 0 ? max(local.existing_funcapps_resource_numbers...) : 0
-
+  
 
   # Check if the file exists and read it
   windows_function_app_names_from_file = fileexists("${path.module}/windows_function_app_names.txt") ? split("\n", file("${path.module}/windows_function_app_names.txt")) : []
@@ -34,7 +38,7 @@ locals {
   windows_function_app_names_count_from_file = length(local.windows_function_app_names_from_file)
 
   # Generate new function app names if needed
-  nwindows_funcapp_names = local.windows_function_app_names_count_from_file < var.windows_funcapp_count ? [for i in range(var.windows_funcapp_count - local.windows_function_app_names_count_from_file) : (format("${var.rg_prefix}-%s%02d", local.effective_environment == "PROD" ? "1${local.effective_applicationname}func" : local.effective_environment == "UAT" ? "4${local.effective_applicationname}func" : "5${local.effective_applicationname}func", local.existing_funcapps_max_resource_numbers + i + 1))] : []
+  nwindows_funcapp_names = local.windows_function_app_names_count_from_file < var.windows_funcapp_count ? [for i in range(var.windows_funcapp_count - local.windows_function_app_names_count_from_file) : "${local.funcappname}${format("%02d", local.existing_funcapps_max_resource_numbers + i + 1)}"] : []
 
   # Concatenate old names with new names
   windows_funcapp_names = concat(local.windows_function_app_names_from_file, local.nwindows_funcapp_names)
