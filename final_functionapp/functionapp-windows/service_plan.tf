ab9c6c667b0490e9f159c9b719b8d98ba5b7a5ff
Ibrahim K <68802843+ibrahimmumgit@users.noreply.github.com>
2024-10-01 10:47:58 +0800

    Update service_plan.tf

--- final_functionapp/functionapp-windows/service_plan.tf
+++ final_functionapp/functionapp-windows/service_plan.tf
@@ -4,7 +4,8 @@ resource "azurerm_service_plan" "aspwindows" {
   count                  = var.create_new_service_plan ? 1 : 0
   name                   = (format("${var.rg_prefix}-%s%02d", local.effective_environment == "PROD" ? "1${local.effective_applicationname}asp" : local.effective_environment == "UAT" ? "4${local.effective_applicationname}asp" : "5${local.effective_applicationname}asp", local.existing_serviceplans_max_resource_numbers + 1))
   resource_group_name    = var.create_new_resource_group ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
-  location               = var.create_new_resource_group ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
+  #location               = var.create_new_resource_group ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
+  location = var.location
   os_type                = "Windows"
   sku_name               = var.sku_size_windows
   zone_balancing_enabled = local.effective_environment == "PROD" && var.zone_redundant
