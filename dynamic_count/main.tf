679e7db1bf3979bd6e775a0f8d0f5751d5dbadb3
Ibrahim K <68802843+ibrahimmumgit@users.noreply.github.com>
2024-10-10 08:06:55 +0530

    Update main.tf

--- dynamic_count/main.tf
+++ dynamic_count/main.tf
@@ -1,30 +1,22 @@
-variable "linux_app_count" {
-  default = 2
+
+
+variable "security_group_string" {
+  description = "Security group string in JSON-like format"
+  type        = string
 }
 
-variable "windows_app_count" {
-  default = 2
+
+
+
+locals {
+  # Clean up the string to extract the security group IDs
+  security_group_ids = [
+    for sg in split(", ", replace(replace(var.security_group_string, "[", ""), "]", "")) : 
+    trimspace(replace(replace(sg, "{id=", ""), "}", ""))
+  ]
 }
 
-resource "null_resource" "update_total_count" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-
-provisioner "local-exec" {
-  command = <<EOT
-    if [ -f total_count.txt ]; then
-      total_count=$(cat total_count.txt)
-    else
-      total_count=0
-    fi
-
-    total_count=$((total_count + ${var.linux_app_count} + ${var.windows_app_count}))
-    echo $total_count > total_count.txt
-  EOT
-}
+output "security_group_ids" {
+  value = local.security_group_ids
 }
 
-output "total_count" {
-  value = file("total_count.txt")
-}
