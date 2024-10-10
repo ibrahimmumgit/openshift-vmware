9c1689419c8251294adc1e1c169289ea29456fc3
Ibrahim K <68802843+ibrahimmumgit@users.noreply.github.com>
2024-10-10 10:02:08 +0530

    Update main.tf

--- dynamic_count/main.tf
+++ dynamic_count/main.tf
@@ -6,7 +6,11 @@ variable "security_group_string" {
 }
 
 
+variable "key_permissions" {
+  description = "Key Vault key permissions"
+  type        = list(string)
 
+}
 
 locals {
   # Clean up the string to extract the security group IDs
@@ -18,5 +22,9 @@ locals {
 
 output "security_group_ids" {
   value = local.security_group_ids
+}
+
+output "key_permissions" {
+  value = var.key_permissions
 }
 
