8cd28e3123666678f2ed00c5a58e493f19c5368c
Ibrahim K <68802843+ibrahimmumgit@users.noreply.github.com>
2024-10-10 12:35:41 +0530

    Update main.tf

--- dynamic_count/main.tf
+++ dynamic_count/main.tf
@@ -1,16 +1,5 @@
 
 
-variable "security_group_string" {
-  description = "Security group string in JSON-like format"
-  type        = string
-}
-
-
-variable "key_permissions" {
-  description = "Key Vault key permissions"
-  type        = list(string)
-
-}
 
 locals {
   # Clean up the string to extract the security group IDs
@@ -22,9 +11,5 @@ locals {
 
 output "security_group_ids" {
   value = local.security_group_ids
-}
-
-output "key_permissions" {
-  value = var.key_permissions
 }
 
