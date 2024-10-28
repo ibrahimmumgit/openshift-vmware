2042a43ffbe255b89f95ece9253a96a9140172ea
Ibrahim K <68802843+ibrahimmumgit@users.noreply.github.com>
2024-10-28 15:06:14 +0530

    Update output.tf

--- final_functionapp/functionapp-linux/output.tf
+++ final_functionapp/functionapp-linux/output.tf
@@ -3,9 +3,29 @@ output "function_app_linux_urls" {
   description = "URLs of the Linux Function Apps"
 }
 
+output "linux_function_app_names_from_file" {
+  value = local.linux_function_app_names_from_file
+}
 
+output "linux_function_app_names_count_from_file" {
+  value = local.linux_function_app_names_count_from_file
+}
 
+output "nlinux_funcapp_names" {
+  value = local.nlinux_funcapp_names
+}
 
+output "linux_funcapp_names" {
+  value = local.linux_funcapp_names
+}
+
+output "truncated_linux_funcapp_names" {
+  value = local.truncated_linux_funcapp_names
+}
+
+output "existing_funcapps_max_resource_numbers" {
+  value = local.existing_funcapps_max_resource_numbers
+}
 
 
 
